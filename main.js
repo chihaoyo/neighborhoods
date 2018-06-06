d16bc39053e9e36fb3cda09a716c035aad8f88e6
chihaoyo <chihaoyo@gmail.com>
2018-06-06 07:00:17 -0400

    201804-structured

--- main.js
+++ main.js
@@ -2,13 +2,16 @@
 // https://segis.moi.gov.tw/STAT/Web/Platform/QueryInterface/STAT_QueryProductView.aspx?pid=436ABDE5C5F32DB5B9927ED0C014D041&spid=7ED8D58E129BC680
 // https://sheethub.com/area.reference.tw/中華民國行政區
 
-const sourceFilePath = './20180205-raw.json'
-const rowsArrayProp = 'RowDataList'
-const colCity = 'COUNTY'
-const colDistrict = 'TOWN'
-const colNeighborhood = 'VILLAGE'
+const sourceFilePath = './opendata10704M030-processed.json'
+const rowsArrayProp = false
+const colCity = 'city'
+const colDistrict = 'district'
+const colNeighborhood = 'neighborhood'
 
-const hoods = require(sourceFilePath)[rowsArrayProp]
+let hoods = require(sourceFilePath)
+if(rowsArrayProp) {
+  hoods = hoods[rowsArrayProp]
+}
 var cities = [...new Set(hoods.map(hood => hood[colCity]))]
 
 var districts = cities.map(city => ({
